032b72cd3f68d9e9476e5ab10a61e6c939bcf637
Nina Verheijen <nina.verheijen@student.uva.nl>
2018-06-27 18:37:40 +0200

    info map

---
+++ maptext.js
@@ -0,0 +1,199 @@
+function getMap() {
+  var x = document.getElementById("myRange")
+  var currentVal = x.value
+  return currentVal
+}
+
+function writeText(gebied) {
+  var year = getMap()
+
+  if (year == "2011") {
+    if (gebied == "Westpoort"){
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 4157,71 <br> gem. gasverbuik: 2790,89 ";
+    }
+    if (gebied == "Nieuw-West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2596,22 <br> gem. gasverbuik: 1375,84 ";
+    }
+    if (gebied == "West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2354,22 <br> gem. gasverbuik: 1245,09 ";
+    }
+    if (gebied == "Noord") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2681,23 <br> gem. gasverbuik: 1492,51 ";
+    }
+    if (gebied == "Centrum") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2951,67 <br> gem. gasverbuik: 1770,24 ";
+    }
+    if (gebied == "Zuid") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2674,06 <br> gem. gasverbuik: 1612,76 ";
+    }
+    if (gebied == "Oost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2546,2 <br> gem. gasverbuik: 1342,73 ";
+    }
+    if (gebied == "Zuidoost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: Geen data <br> gem. gasverbuik: Geen data ";
+    }
+  }
+
+  if (year == "2012") {
+    if (gebied == "Westpoort"){
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 3757,4 <br> gem. gasverbuik: 2758,21 ";
+    }
+    if (gebied == "Nieuw-West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2588,44 <br> gem. gasverbuik: 1342,99 ";
+    }
+    if (gebied == "West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2374,76 <br> gem. gasverbuik: 1214,46 ";
+    }
+    if (gebied == "Noord") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2674,1 <br> gem. gasverbuik: 1457,2 ";
+    }
+    if (gebied == "Centrum") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2948,14 <br> gem. gasverbuik: 1740,07 ";
+    }
+    if (gebied == "Zuid") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2695,51 <br> gem. gasverbuik: 1573,84 ";
+    }
+    if (gebied == "Oost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2558,59 <br> gem. gasverbuik: 1311,89 ";
+    }
+    if (gebied == "Zuidoost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: Geen data <br> gem. gasverbuik: Geen data ";
+    }
+  }
+
+  if (year == "2013") {
+    if (gebied == "Westpoort"){
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 4098,0 <br> gem. gasverbuik: 2819,29 ";
+    }
+    if (gebied == "Nieuw-West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2626,41 <br> gem. gasverbuik: 1311,36 ";
+    }
+    if (gebied == "West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2378,34 <br> gem. gasverbuik: 1194,62 ";
+    }
+    if (gebied == "Noord") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2655,05 <br> gem. gasverbuik: 1418,06 ";
+    }
+    if (gebied == "Centrum") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2929,05 <br> gem. gasverbuik: 1711,47 ";
+    }
+    if (gebied == "Zuid") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2691,39 <br> gem. gasverbuik: 1531,17 ";
+    }
+    if (gebied == "Oost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 2583,81 <br> gem. gasverbuik: 1293,41 ";
+    }
+    if (gebied == "Zuidoost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: Geen data <br> gem. gasverbuik: Geen data ";
+    }
+  }
+
+  if (year == "2014") {
+    if (gebied == "Westpoort"){
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 242 <br> gem. Stroomverbruik: 3630,5 <br> gem. gasverbuik: 2716,52 ";
+    }
+    if (gebied == "Nieuw-West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 144089 <br> gem. Stroomverbruik: 2596,32 <br> gem. gasverbuik: 1297,55 ";
+    }
+    if (gebied == "West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 141098 <br> gem. Stroomverbruik: 2356,93 <br> gem. gasverbuik: 1183,71 ";
+    }
+    if (gebied == "Noord") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 89906 <br> gem. Stroomverbruik: 2612,83 <br> gem. gasverbuik: 1416,81 ";
+    }
+    if (gebied == "Centrum") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 86099 <br> gem. Stroomverbruik: 2945,81 <br> gem. gasverbuik: 1715,94 ";
+    }
+    if (gebied == "Zuid") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 139523 <br> gem. Stroomverbruik: 2676,62 <br> gem. gasverbuik: 1523,39 ";
+    }
+    if (gebied == "Oost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 126157 <br> gem. Stroomverbruik: 2568,41 <br> gem. gasverbuik: 1279,29 ";
+    }
+    if (gebied == "Zuidoost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 84071 <br> gem. Stroomverbruik: Geen data <br> gem. gasverbuik: Geen data ";
+    }
+  }
+
+  if (year == "2015") {
+    if (gebied == "Westpoort"){
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 202 <br> gem. Stroomverbruik: 3928,25 <br> gem. gasverbuik: 2508,11 ";
+    }
+    if (gebied == "Nieuw-West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 146801 <br> gem. Stroomverbruik: 2541,57 <br> gem. gasverbuik: 1267,53 ";
+    }
+    if (gebied == "West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 142832 <br> gem. Stroomverbruik: 2286,25 <br> gem. gasverbuik: 1139,47 ";
+    }
+    if (gebied == "Noord") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 91324 <br> gem. Stroomverbruik: 2538,54 <br> gem. gasverbuik: 1375,8 ";
+    }
+    if (gebied == "Centrum") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 86418 <br> gem. Stroomverbruik: 2841,21 <br> gem. gasverbuik: 1646,63 ";
+    }
+    if (gebied == "Zuid") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 141438 <br> gem. Stroomverbruik: 2596,08 <br> gem. gasverbuik: 1475,13 ";
+    }
+    if (gebied == "Oost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 128690 <br> gem. Stroomverbruik: 2507,52 <br> gem. gasverbuik: 1225,34 ";
+    }
+    if (gebied == "Zuidoost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 84567 <br> gem. Stroomverbruik: 4544,0 <br> gem. gasverbuik: 1046,0 ";
+    }
+  }
+
+  if (year == "2016") {
+    if (gebied == "Westpoort"){
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 200 <br> gem. Stroomverbruik: 3552,33 <br> gem. gasverbuik: 2469,48 ";
+    }
+    if (gebied == "Nieuw-West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 149397 <br> gem. Stroomverbruik: 2488,29 <br> gem. gasverbuik: 1238,95 ";
+    }
+    if (gebied == "West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 143964 <br> gem. Stroomverbruik: 2244,68 <br> gem. gasverbuik: 1113,69 ";
+    }
+    if (gebied == "Noord") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 92917 <br> gem. Stroomverbruik: 2508,24 <br> gem. gasverbuik: 1345,75 ";
+    }
+    if (gebied == "Centrum") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 86499 <br> gem. Stroomverbruik: 2792,52 <br> gem. gasverbuik: 1616,11 ";
+    }
+    if (gebied == "Zuid") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 143258 <br> gem. Stroomverbruik: 2555,3 <br> gem. gasverbuik: 1442,7 ";
+    }
+    if (gebied == "Oost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 132421 <br> gem. Stroomverbruik: 2475,29 <br> gem. gasverbuik: 1203,41 ";
+    }
+    if (gebied == "Zuidoost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 86057 <br> gem. Stroomverbruik: 2691,72 <br> gem. gasverbuik: 1279,81 ";
+      }
+    }
+
+  if (year == "2017") {
+    if (gebied == "Westpoort"){
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 192 <br> gem. Stroomverbruik: 3552,33 <br> gem. gasverbuik: 2602,14 ";
+    }
+    if (gebied == "Nieuw-West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 151677 <br> gem. Stroomverbruik: 2488,29 <br> gem. gasverbuik: 1230,27 ";
+    }
+    if (gebied == "West") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 143842 <br> gem. Stroomverbruik: 2244,68 <br> gem. gasverbuik: 1093,01 ";
+    }
+    if (gebied == "Noord") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 94766 <br> gem. Stroomverbruik: 2508,24 <br> gem. gasverbuik: 1346,6 ";
+    }
+    if (gebied == "Centrum") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 86422 <br> gem. Stroomverbruik: 2792,52 <br> gem. gasverbuik: 1597,14 ";
+    }
+    if (gebied == "Zuid") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 144432 <br> gem. Stroomverbruik: 2555,3 <br> gem. gasverbuik: 1430,99 ";
+    }
+    if (gebied == "Oost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 135767 <br> gem. Stroomverbruik: 2475,29 <br> gem. gasverbuik: 1176,61 ";
+    }
+    if (gebied == "Zuidoost") {
+      document.getElementById("desc").innerHTML = gebied + "<br> Inwoners: 87854 <br> gem. Stroomverbruik: 2691,72 <br> gem. gasverbuik: 1288,52 ";
+    }
+  }
+
+}
